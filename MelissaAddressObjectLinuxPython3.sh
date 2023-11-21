b2d938abf36212c3ed35d9877057693c6f50a89f
Melissa <>
2023-11-21 09:15:17 -0800

    Tested successfully using Melissa data files for 2023.11

--- MelissaAddressObjectLinuxPython3.sh
+++ MelissaAddressObjectLinuxPython3.sh
@@ -73,7 +73,7 @@ done
 
 ######################### Config ###########################
 
-RELEASE_VERSION='2023.10'
+RELEASE_VERSION='2023.11'
 ProductName="DQ_ADDR_DATA"
 
 # Uses the location of the .sh file 
